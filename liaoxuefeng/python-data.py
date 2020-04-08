736dab6f1de4363c16463d8644a91bfde81538cc
light-Life <53685855+light-Life@users.noreply.github.com>
2020-04-08 15:22:38 +0800

    数据来源：廖雪峰的官方网站

--- liaoxuefeng/python-data.py
+++ liaoxuefeng/python-data.py
@@ -1,55 +1,55 @@
-＃！/ usr / bin / pythoy
-＃-*-编码：utf-8-*-
-＃时间：2020.3.19
-进口 的请求，重新
-进口 pyecharts。选项 为 OPTS
-从 bs4  导入 BeautifulSoup
-来自 pyecharts。图表 导入 线
+# !/usr/bin/pythoy
+# -*- coding:utf-8 -*-
+# time: 2020.3.19
+import requests,re
+import pyecharts.options as opts
+from bs4 import BeautifulSoup
+from pyecharts.charts import Line
 
-标头 = { 'User-Agent'：'Mozilla / 5.0' }
-url  =  'https://www.liaoxuefeng.com/wiki/1016959663602400'
-响应 =  请求。get（url，headers = headers）
+headers = {'User-Agent':'Mozilla/5.0'}
+url = 'https://www.liaoxuefeng.com/wiki/1016959663602400'
+response = requests.get(url,headers=headers)
 
-inde  =  re。findall（'<li。*？id =“ off-（。*？）”。*？<a。*？href =“（。*？）”>（。*？）</a>'，响应。文本，重新。小号）
-名称 = []
-阅读 = []
-对于 东印度 在 不知疲倦：
-    id  =  str（indes [ 1 ]）
-    名字。追加（indes [ 2 ]）
-    print（'正在分析：'，indes [ 2 ]）
-    URLID  =  'https://www.liaoxuefeng.com' + ID
-    响应 =  请求。get（urlid，headers = headers）
-    如果 回应。status_code  ==  200：
-        inde  =  re。的findall（'<跨度>读取：（*）</跨度>？'，响应。文本）
-        对于 东印度 在 不知疲倦：
-            阅读。追加（indes）
+inde = re.findall('<li.*?id="off-(.*?)".*?<a.*?href="(.*?)">(.*?)</a>',response.text,re.S)
+name = []
+Read = []
+for indes in inde:
+    id = str(indes[1])
+    name.append(indes[2])
+    print('正在分析:',indes[2])
+    urlid = 'https://www.liaoxuefeng.com'+id
+    response = requests.get(urlid,headers=headers)
+    if response.status_code == 200:
+        inde = re.findall('<span>Reads:(.*?)</span>',response.text)
+        for indes in inde:
+            Read.append(indes)
 
-＃建立可视化分析图
-（
-    线（init_opts = OPTS。InitOpts（宽度= “1920px” ，高度= “900px” ））
-    。add_xaxis（xaxis_data = name）
-    。add_yaxis（
-        series_name = “热力值”，＃坐标轴指针的文本标签
-        y_axis = 读取，
-        areastyle_opts = opts。AreaStyleOpts（opacity = 0.5），＃填充颜色深度，替换颜色
-        label_opts = opts。LabelOpts（is_show = False），＃标签配置项，显示标签数字
-    ）
-    。set_global_opts（
-        title_opts = 选择。TitleOpts（
-            title = “ Python在线学习热力值”，＃主标题文本，支持使用\ n换行
-            subtitle = “数据来自廖雪峰Python教程”，＃副标题文本，支持使用\ n换行。
-            pos_left = “ center”，＃位置自动对齐（垂直）
-            pos_top = “ top”，＃位置自动对齐（上侧）
-        ），
-        tooltip_opts  =  opts。TooltipOpts（trigger = “ axis”，axis_pointer_type = “ cross”），＃提示框组件
-        legend_opts  =  opts。LegendOpts（pos_left = “ left”），＃坐标轴指示器位置（左）
-        datazoom_opts  = [ ＃显示组件
-            选择。DataZoomOpts（range_start = 0，range_end = 100），＃数据窗口范围的起始百分比。
-            选择。DataZoomOpts（type_ = “内部”，RANGE_START = 0，RANGE_END = 100）， ＃组件类型，长数据建议选用内部。同上
-        ]，
-        xaxis_opts  =  opts。AxisOpts（type_ = “类别”，boundary_gap = 假）， ＃'类别'：类目轴，适用于离散的类目数据，为该类型时必须通过数据设置类目数据。
-        yaxis_opts  =  opts。AxisOpts（名称= “热度”，type_ = “值”，MAX_ = 8.5亿）， ＃'值'：数值轴，适用于连续数据。
-    ）
-    。render（“ python.html”）＃路径
-）
-打印（' \ n \ t分析完成！'）
+#建立可视化分析图
+(
+    Line(init_opts=opts.InitOpts(width="1920px", height="900px"))
+    .add_xaxis(xaxis_data=name)
+    .add_yaxis(
+        series_name="热力值",#坐标轴指示器的文本标签
+        y_axis=Read,
+        areastyle_opts=opts.AreaStyleOpts(opacity=0.5),#填充颜色深度,默认颜色
+        label_opts=opts.LabelOpts(is_show=False),#标签配置项,显示标签数字
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(
+            title="Python在线学习热力值",# 主标题文本，支持使用 \n 换行
+            subtitle="数据来自廖雪峰Python教程", # 副标题文本，支持使用 \n 换行。
+            pos_left="center",#位置自动对齐（左侧）
+            pos_top="top",#位置自动对齐（上侧）
+        ),
+        tooltip_opts = opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),# 提示框组件
+        legend_opts = opts.LegendOpts(pos_left="left"),#坐标轴指示器位置（左）
+        datazoom_opts = [#显示组件
+            opts.DataZoomOpts(range_start=0, range_end=100),# 数据窗口范围的起始百分比。# 数据窗口范围的结束百分比
+            opts.DataZoomOpts(type_="inside", range_start=0, range_end=100),#组件类型，长数据建议选用inside。同上
+        ],
+        xaxis_opts = opts.AxisOpts(type_="category", boundary_gap=False),# 'category': 类目轴，适用于离散的类目数据，为该类型时必须通过 data 设置类目数据。
+        yaxis_opts = opts.AxisOpts(name="热度", type_="value", max_=850000000),# 'value': 数值轴，适用于连续数据。
+    )
+    .render("python.html")#路径
+)
+print('\n\t分析完成！')
